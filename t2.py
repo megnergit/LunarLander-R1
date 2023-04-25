093f335bd4306ba5cc575d0299dc4c3f740d59aa
Meg <miwa.egner@gmail.com>
2023-04-25 11:06:52 +0200

    update requirement.txt

--- t2.py
+++ t2.py
@@ -16,7 +16,8 @@ from stable_baselines3.common.utils import set_random_seed
 import logging
 
 # rom tqdm.rich import tqdm
-from tqdm import rich
+# from tqdm import rich
+import tqdm
 import pretty_errors
 import pdb
 # import pyglet
